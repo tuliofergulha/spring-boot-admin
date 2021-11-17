6016e4b660d6392d64fab4ecae2a6d6a8f3ccf08
Túlio Fergulha <tfergulha@gmail.com>
2021-11-16 22:10:40 -0300

    Configurado admin server

--- src/main/java/br/com/tfergulha/springbootadmin/SpringBootAdminApplication.java
+++ src/main/java/br/com/tfergulha/springbootadmin/SpringBootAdminApplication.java
@@ -1,13 +1,18 @@
 package br.com.tfergulha.springbootadmin;
 
 import org.springframework.boot.SpringApplication;
-import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+import org.springframework.context.annotation.Configuration;
 
-@SpringBootApplication
+import de.codecentric.boot.admin.server.config.EnableAdminServer;
+
+@Configuration
+@EnableAutoConfiguration
+@EnableAdminServer
 public class SpringBootAdminApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(SpringBootAdminApplication.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(SpringBootAdminApplication.class, args);
+    }
 
 }
